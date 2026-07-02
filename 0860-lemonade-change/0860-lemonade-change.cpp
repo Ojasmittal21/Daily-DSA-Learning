3c808bd3b6f9f4b26d42bc0bc80cdeb72afc0ddc
OM_Coder <107555684+Ojasmittal21@users.noreply.github.com>
2026-07-02 20:04:55 +0530

    Time: 0 ms (100%), Space: 87.2 MB (45.34%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        unordered_map<int,int> mp;
+        if(bills[0]!=5) return false; 
+        for(int i=0;i<bills.size();i++){
+            mp[bills[i]]++;
+            if(bills[i]==10){
+                if(mp[5]==0) return false;
+                else mp[5]--;
+            }
+            else if(bills[i]==20){
+                if(mp[5]==0) return false;
+                else if(mp[10]==0 && mp[5]<3) return false;
+                else{
+                    if(mp[10]==0){
+                        mp[5]-=3;
+                    }
+                    else{
+                        mp[10]--;
+                        mp[5]--;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
